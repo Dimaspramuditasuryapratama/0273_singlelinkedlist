4fb9062a5474cca3271c6ef4fc2932283aa02dc7
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-09 09:44:48 +0700

    Refactor LinkedList class to improve addNode method and enhance duplicate handling

--- file1.cpp
+++ file1.cpp
@@ -2,100 +2,120 @@
 #include <string.h>
 using namespace std;
 
-class Node 
+class Node
 {
-    public:
-        int noMhs;
-        Node *next;
+public:
+    int noMhs;
+    Node *next;
 };
 
 class LinkedList
 {
     Node *START;
 
-    public:
-        LinkedList()
-        {
-            START = NULL;
-        }
-        void addNode()
-        {
-            int nim;
-            cout << "\nMasukkan Nomor Mahasiswa: ";
-            cin >> nim;
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
 
-            Node *nodeBaru = new Node;
-            nodeBaru->noMhs = nim;
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
 
-            if (START == NULL || nim <= START->noMhs)
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
             {
-                if ((START != NULL) && (nim == START->noMhs))
-                {
-                    cout << "\nDuplikasi noMhs tidak diijinkan\n";
-                    return;
-                }
-                nodeBaru->next = START;
-                START = nodeBaru;
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
                 return;
             }
-            Node *previous = START;
-            Node *current = START;
-    
-            while ((current != NULL) && (nim >= current->noMhs))
-            {
-                if (nim == current->noMhs)
-                {
-                    cout << "\nDuplikasi noMhs tidak diijinkan\n";
-                    return;
-                }
-                previous = current;
-                current = current->next;
-            }
-            nodeBaru->next = current;
-            previous->next = nodeBaru;
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
         }
-        bool listEmpty()
-        {
-            return (START == NULL);
-        }
+        Node *previous = START;
+        Node *current = START;
 
-        bool Search(int nim, Node **previous, Node **current)
+        while ((current != NULL) && (nim >= current->noMhs))
         {
-            *previous = START;
-            *current = START;
-            while ((*current != NULL) && (nim > (*current)->noMhs))
+            if (nim == current->noMhs)
             {
-                *previous = *current;
-                *current = (*current)->next;
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
             }
-            return (*current != NULL);
+            previous = current;
+            current = current->next;
         }
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
+    bool listEmpty()
+    {
+        return (START == NULL);
+    }
 
-        bool delNode(int nim)
+    bool Search(int nim, Node **previous, Node **current)
+    {
+        *previous = START;
+        *current = START;
+        while ((*current != NULL) && (nim > (*current)->noMhs))
         {
-            Node *current, *previous;
-            if (!Search(nim, &previous, &current))
+            *previous = *current;
+            *current = (*current)->next;
+        }
+        return (*current != NULL);
+    }
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (!Search(nim, &previous, &current))
             return false;
 
-            if (current == START)
+        if (current == START)
             START = START->next;
-            else
+        else
             previous->next = current->next;
 
-            delete current;
-            return true;            
-        }
-        void traverse()
+        delete current;
+        return true;
+    }
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nData didalam list adalah:\n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
             {
-                if (listEmpty())
-                {
-                    cout << "\nData didalam list adalah:\n";
-                    Node *currentNode = START;
-                    while (currentNode != NULL)
-                    {cout << currentNode->noMhs << endl;
-                    currentNode = currentNode->next;
-                    }
-                    cout << endl;
-                }
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
             }
+            cout << endl;
+        }
+    }
 };
+
+int main()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+    do
+    {
+        cout << "Menu" << endl;
+        cout << "1. Menambah data kedalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data didalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout << endl
+             << "Masukkan pilihan (1-5): ";
+        cin >> ch;
+        
+}
