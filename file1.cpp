6a292e510a77b450ff58309d0fae5bda9ac37d17
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-09 09:47:13 +0700

    Add search functionality to LinkedList with user feedback for empty list and search results

--- file1.cpp
+++ file1.cpp
@@ -143,6 +143,33 @@ int main()
         }
         break;
         
+        case '3':
+        {
+            mhs.traverse();
+        }
+        break;
+        case '4':
+        {
+            if (mhs.listEmpty() == true)
+            {
+                cout << "\nList Kosong\n";
+                break;
+            }
+            Node *previous, *current;
+            cout << endl
+                << "Masukkan no mahasiswa yang dicari : ";
+                cin >> nim;
+                if (mhs.Search(nim, &previous, &current) == false)
+                cout << endl
+                    << "Data tidak ditemukan" << endl;
+                    else
+                    {
+                        cout << endl
+                            << "Data ditemukan" << endl;
+                            cout << "\nNomor Mahasiswa: " << current->noMhs << endl;
+                            cout << "\n";
+                    }
+        }
         }
         
     }while (ch != '5');
