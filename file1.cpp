625df6f469865d581575d5275f587b8a7df44694
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-09 08:53:10 +0700

    Add traverse method to LinkedList class for displaying list contents

--- file1.cpp
+++ file1.cpp
@@ -85,4 +85,17 @@ class LinkedList
             delete current;
             return true;            
         }
+        void traverse()
+            {
+                if (listEmpty())
+                {
+                    cout << "\nData didalam list adalah:\n";
+                    Node *currentNode = START;
+                    while (currentNode != NULL)
+                    {cout << currentNode->noMhs << endl;
+                    currentNode = currentNode->next;
+                    }
+                    cout << endl;
+                }
+            }
 };
